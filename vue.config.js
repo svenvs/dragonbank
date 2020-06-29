e74afecca9687a29ea4fa093a40e57e2b3df5181
Sven van Straalen <ssgvanstraalen@gmail.com>
2020-06-29 21:04:25 +0200

    added pwa stuff

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,19 @@ console.log(process.env.NODE_ENV)
 module.exports = {
   outputDir: 'docs',
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'prod' ? '/dragonbank/' : '/'
+  publicPath: process.env.NODE_ENV === 'prod' ? '/dragonbank/' : '/',
+  pwa: {
+    name: 'prototypePWA',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/service-worker.js',
+      // ...other Workbox options...
+    },
+  },
 }
