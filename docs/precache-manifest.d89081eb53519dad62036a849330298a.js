8d2207bb4e9fb808581fb4fb6f85d0f85f75161d
Sven van Straalen <ssgvanstraalen@gmail.com>
2020-06-29 20:15:46 +0200

    added docs to host static files

---
+++ docs/precache-manifest.d89081eb53519dad62036a849330298a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45279fd21a0548b62069",
+    "url": "/css/app.80ef5267.css"
+  },
+  {
+    "revision": "e6f0609fa7de901edbad",
+    "url": "/css/chunk-vendors.edf780ca.css"
+  },
+  {
+    "revision": "0dd3a7221b2379621a4d8cbf767307b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45279fd21a0548b62069",
+    "url": "/js/app.1f694d4e.js"
+  },
+  {
+    "revision": "e6f0609fa7de901edbad",
+    "url": "/js/chunk-vendors.ef539696.js"
+  },
+  {
+    "revision": "62803d343f88378a69dafabd9f291798",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
